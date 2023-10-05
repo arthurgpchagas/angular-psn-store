aec3728bf494795e866a8901420d5c4bc1a887e5
Arthur <arthurchagas01@gmail.com>
2023-10-05 09:22:18 -0300

    Complete prroject - Angular look-alike PSN

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -7,7 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
-  constructor() { }
+	@Input()
+	gameCover:string= ""
+
+	@Input()
+	gameLabel:string =""
+
+	@Input()
+	gameType:string = "Digital | PS4"
+
+	@Input()
+	gamePrice:string = "R$ 399,99"
+
+	constructor() { }
 
   ngOnInit(): void {
   }
